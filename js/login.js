43fb153a9292f8cd4302f37ab49e56d46b83a63d
jeffwiyan <jeffwiyan@gmail.com>
2025-10-03 20:36:22 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,13 @@
+// Login page logic
+$(document).ready(function() {
+    $('#login-form').on('submit', function(e) {
+        e.preventDefault();
+        if ($('#username').val() === 'admin' && $('#password').val() === '12345') {
+            localStorage.setItem('isLoggedIn', 'true');
+            window.location.href = 'dashboard.html';
+        } else {
+            $('#login-error').text('Username atau password salah.');
+        }
+    });
+});
 
